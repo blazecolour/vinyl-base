69c9fa1d1a9aaf8bcc52d5a9ad8786514ea60ce2
blazecolour <936x936@gmail.com>
2017-04-18 20:31:28 +0300

    add functions

--- functions.php
+++ functions.php
@@ -1,28 +1,19 @@
 <?php
-        /**
 
-         * Debug a variable with stop app
+function dd() {
+        if (!YII_DEBUG) return;
+        array_map(function ($x){
+                echo \yii\helpers\VarDumper::dump($x, 10, true);
+        }, func_get_args());
 
-         */
+        Yii::$app->end();
+}
 
-        function dd() {
-                if (!YII_DEBUG) return;
-                array_map(function ($x){
-                        echo \yii\helpers\VarDumper::dump($x, 10, true);
-                }, func_get_args());
 
-                Yii::$app->end();
-        }
 
-        /**
-
-         * Debug a variable without stop app
-
-         */
-
-        function d() {
-                if (!YII_DEBUG) return;
-                array_map(function ($x){
-                        echo \yii\helpers\VarDumper::dump($x, 10, true);
-                }, func_get_args());
-        }
+function d() {
+        if (!YII_DEBUG) return;
+        array_map(function ($x){
+                echo \yii\helpers\VarDumper::dump($x, 10, true);
+        }, func_get_args());
+}
